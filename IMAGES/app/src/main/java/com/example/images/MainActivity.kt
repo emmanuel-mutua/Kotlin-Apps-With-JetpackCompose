9f37f06258cc4edb8c7e32224979971fa02e2bc0
Emmutua <emmamulwa2020@gmail.com>
2023-01-23 18:23:37 +0300

    Jetpack Compose Apps

--- IMAGES/app/src/main/java/com/example/images/MainActivity.kt
+++ IMAGES/app/src/main/java/com/example/images/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.images
 
+import android.media.Image
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.ColorRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,7 +26,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.images.ui.theme.IMAGESTheme
+import java.lang.reflect.Modifier
 
 
 class MainActivity : ComponentActivity() {
@@ -44,13 +43,19 @@ class MainActivity : ComponentActivity() {
 fun MyApp() {
     val image = painterResource(id = R.drawable.br)
     Scaffold(
-        topBar = { TopAppBarCompose() }
+        topBar = { TopAppBarCompose() },
+
+        //  bottomBar = { bottomAppBarCompose() }
+
     ) {
         Box {
-            Image(modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(),
-                painter = image, contentDescription = null, contentScale = ContentScale.Crop)
+
+            Image(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                painter = image, contentDescription = null, contentScale = ContentScale.Crop
+            )
             Message()
         }
     }
@@ -59,48 +64,96 @@ fun MyApp() {
 
 @Composable
 fun Message() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp),
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top) {
+        verticalArrangement = Arrangement.Top
+    ) {
         Text(
             text = stringResource(R.string.happy_birthday_msg),
             style = MaterialTheme.typography.h5,
-            fontWeight = FontWeight.Bold)
-        Text(modifier = Modifier
-            .fillMaxSize()
-            .wrapContentWidth(Alignment.CenterHorizontally)
-            .padding(top = 10.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(top = 10.dp),
             text = stringResource(R.string.msg_from),
             style = MaterialTheme.typography.subtitle1,
-            fontStyle = FontStyle.Italic)
+            fontStyle = FontStyle.Italic
+        )
     }
 }
 
 @Composable
 fun TopAppBarCompose() {
     val context = LocalContext.current
-    TopAppBar(
-        title = {
-            Text(text = stringResource(R.string.appbar_title),
-                fontSize = 20.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis)
-        },
-        navigationIcon = { IconButton(onClick = {Toast.makeText(context, "Menu",Toast.LENGTH_SHORT).show() }) {
-            Icon(Icons.Default.Menu, contentDescription = "Menu")
-        }},
-        actions = {
-            IconButton(onClick = { Toast.makeText(context, "Add", Toast.LENGTH_SHORT)}) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
+    var showTextField by remember {
+        mutableStateOf(false)
+    }
+    if (!showTextField) {
+        TopAppBar(
+            modifier = Modifier.border(width = 0.dp, color = Color.Transparent),
+            title = {
+                Text(
+                    text = stringResource(R.string.appbar_title),
+                    fontSize = 20.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            elevation = 0.dp,
+            navigationIcon = {
+                IconButton(onClick = {
+                    Toast.makeText(context, "Menu", Toast.LENGTH_SHORT).show()
+                }) {
+                    Icon(Icons.Default.Menu, contentDescription = "Menu")
+                }
+            },
+            actions = {
+                IconButton(onClick = {
+                    Toast.makeText(context, "Add", Toast.LENGTH_SHORT).show()
+                }) {
+                    Icon(Icons.Default.Add, contentDescription = "Add")
+                }
 
-            IconButton(onClick = { Toast.makeText(context, "Search", Toast.LENGTH_SHORT) }) {
-                Icon(Icons.Default.Search, contentDescription = "Add")
-            }
-        },
-        backgroundColor = Color.Transparent,
-        contentColor = Color.Magenta
-    )
+                IconButton(onClick = {
+                    Toast.makeText(context, "Search", Toast.LENGTH_SHORT).show()
+                    showTextField = true
+                }) {
+                    Icon(Icons.Default.Search, contentDescription = "Search")
+                }
+                IconButton(onClick = {
+                    Toast.makeText(context, "more", Toast.LENGTH_SHORT).show()
+                }) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "MoreVert")
+                }
+            },
+            backgroundColor = Color.Transparent,
+            contentColor = Color.Magenta,
+        )
+    } else {
+        TopAppBar(modifier = Modifier.border(width = 0.dp, color = Color.Transparent),
+            backgroundColor = Color.Transparent,
+            elevation = 0.dp) {
+            TextField(
+                leadingIcon = {
+                    Icon(
+                        modifier = Modifier.clickable { showTextField = false },
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = null
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {})
+        }
+    }
+
+
 }
+
+

262fb8aede4df4c93eb128d62b2fedbbf6a15649
Emmanuel Mutua <emmamulwa2020@gmail.com>
2023-03-21 22:10:36 +0300

    add UiState, use sealed interface

--- Resources/app/src/main/java/com/example/resources/ui/theme/UiState.kt
+++ Resources/app/src/main/java/com/example/resources/ui/theme/UiState.kt
@@ -1,2 +1,18 @@
 package com.example.resources.ui.theme
 
+import com.example.resources.model.*
+import com.example.resources.model.users.Users
+
+sealed interface UiState {
+    data class Success(
+        val users: Users,
+        val albums: Albums,
+        val photos: Photos,
+        val posts: Posts,
+        val comments: Comments,
+        val todos: Todos
+    ) : UiState
+    object Loading : UiState
+    object Error : UiState
+
+}
